02803012bc7bf5e8527aac70d04af9521df03de9
Mahadi Hasan <mahadih137@gmail.com>
2023-12-14 23:59:31 +0600

    pagination implemented

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,9 +10,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearchInputChange = useDebouncedCallback((term: string) => {
-    console.log(`Searching.... ${term}`);
+    // console.log(`Searching.... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+
+    params.set('page', '1');
 
     if (term) {
       params.set('query', term);
