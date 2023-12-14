d1dd7b16250771580c1698e7411ac94e201b1d0e
Mahadi Hasan <mahadih137@gmail.com>
2023-12-15 01:09:16 +0600

    delete product functionality implemented - chapter 13 complete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,4 @@
+import { DeleteInvoice } from '@/app/ui/invoices/buttons';
 "use server"
 
 import { z } from 'zod';
@@ -53,4 +54,11 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
+}
+
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+
+    revalidatePath("/dashboard/invoices");
 }
